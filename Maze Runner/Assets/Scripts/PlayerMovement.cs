29ecf14eac38cc863cceb737f248bc1b50a5c29a
Mikey Roush <mikeyroush@client-10-228-177-39.tamulink.tamu.edu>
2020-05-28 16:42:33 -0500

    added player, obstacles and score

---
+++ Maze Runner/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 50f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    // Fixed Update is smoother when messing with physics
+    void FixedUpdate()
+    {
+        //use deltaTime to fix the magnitude of the force
+        rb.AddForce(0,0,forwardForce*Time.deltaTime);
+
+        //input can be improved
+        if(Input.GetKey("a")){
+            rb.AddForce(-sidewaysForce*Time.deltaTime,0,0,ForceMode.VelocityChange);
+        }
+
+        if(Input.GetKey("d")){
+            rb.AddForce(sidewaysForce*Time.deltaTime,0,0,ForceMode.VelocityChange);
+        }
+    }
+}
